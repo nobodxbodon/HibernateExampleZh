2be126b03bc557340a6621af8b397e518ce347ae
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-04-18 23:06:38 -0700

    创建数据表, 带索引

---
+++ 数据库/客户.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `演示`.`客户`;
+CREATE TABLE  `演示`.`客户` (
+  `客户_ID` bigint(20) unsigned COLLATE utf8_unicode_ci NOT NULL AUTO_INCREMENT,
+  `姓名` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `创建日期` datetime COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`客户_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
+
+ALTER TABLE `演示`.`客户`
+ADD INDEX `姓名_索引` (`姓名` ASC) ;
